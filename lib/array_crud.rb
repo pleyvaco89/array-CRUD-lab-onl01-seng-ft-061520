9b53b56da1355fbb6a49421bb7c6f6188b22a3c4
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-18 01:52:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 card_faces = ["spades", "clovers", "hearts", "diamonds"]
-card_faces << "joker"
+card_faces << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
